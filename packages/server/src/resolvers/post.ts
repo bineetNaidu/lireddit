648e34978240bfda6426cb9039b03d37d6f5383e
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-27 21:15:17 +0530

    ✨ add textSnippet field for post

--- packages/server/src/resolvers/post.ts
+++ packages/server/src/resolvers/post.ts
@@ -9,6 +9,8 @@ import {
   Ctx,
   UseMiddleware,
   Int,
+  FieldResolver,
+  Root,
 } from 'type-graphql';
 import { MyContext } from 'src/types';
 import { isAuth } from '../middlewares/isAuth';
@@ -23,8 +25,13 @@ class PostInput {
   text: string;
 }
 
-@Resolver()
+@Resolver(Post)
 export class PostResolver {
+  @FieldResolver(() => String)
+  textSnippet(@Root() root: Post) {
+    return root.text.slice(0, 50) + '...';
+  }
+
   @Query(() => [Post])
   posts(
     @Arg('limit', () => Int) limit: number,
