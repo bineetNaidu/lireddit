c6d50d5c55d150aff16028c58d0ded784e6f2f48
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-28 15:11:15 +0530

    🌟 fetching post relations

--- packages/server/src/resolvers/post.ts
+++ packages/server/src/resolvers/post.ts
@@ -16,6 +16,7 @@ import {
 import { MyContext } from 'src/types';
 import { isAuth } from '../middlewares/isAuth';
 import { getConnection } from 'typeorm';
+// import { User } from 'src/entities/User';
 
 @InputType()
 class PostInput {
@@ -49,19 +50,46 @@ export class PostResolver {
     // 20 -> 21
     const realLimit = Math.min(50, limit);
     const reaLimitPlusOne = realLimit + 1;
-    const qb = getConnection()
-      .getRepository(Post)
-      .createQueryBuilder('p')
-      .orderBy('"createdAt"', 'DESC')
-      .take(reaLimitPlusOne);
+    // const qb = getConnection()
+    //   .getRepository(Post)
+    //   .createQueryBuilder('p')
+    //   .innerJoinAndSelect('p.creatorId', 'u', 'u.id = p."creatorId"')
+    //   .orderBy('p."createdAt"', 'DESC')
+    //   .take(reaLimitPlusOne);
+
+    // if (cursor) {
+    //   qb.where('p."createdAt" < :cursor', {
+    //     cursor: new Date(parseInt(cursor)),
+    //   });
+    // }
+    // const posts = await qb.getMany();
+
+    const replacements: any[] = [reaLimitPlusOne];
 
     if (cursor) {
-      qb.where('"createdAt" < :cursor', {
-        cursor: new Date(parseInt(cursor)),
-      });
+      replacements.push(new Date(parseInt(cursor)));
     }
 
-    const posts = await qb.getMany();
+    const posts = await getConnection().query(
+      `
+      select
+      p.*,
+      json_build_object(
+        'id', u.id,
+        'username', u.username,
+        'email', u.email
+      ) as creator
+      from post as p
+      inner join public.user as u
+      on u.id = p."creatorId"
+      ${cursor ? `where p."createdAt" < $2` : ''}
+      order by p."createdAt" DESC
+      limit $1
+    `,
+      replacements
+    );
+
+    console.log(posts);
 
     const hasMore = posts.length === reaLimitPlusOne;
     return {
